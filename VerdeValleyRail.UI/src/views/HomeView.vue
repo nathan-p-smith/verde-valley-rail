ba82f5263837b3a711c9316f0e24168e1e7b6306
Nathan Smith <nathpsmith@gmail.com>
2023-08-05 12:09:59 -0400

    Got search grid displaying.

--- VerdeValleyRail.UI/src/views/HomeView.vue
+++ VerdeValleyRail.UI/src/views/HomeView.vue
@@ -1,10 +1,4 @@
 <script setup>
-import api from '@/services/VerdeValleyRailApi';
-
-var trip = await api.getTrip(111);
-
-console.log(trip);
-
 import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
--- VerdeValleyRail.UI/src/views/TripsView.vue
+++ VerdeValleyRail.UI/src/views/TripsView.vue
@@ -1,13 +1,58 @@
-<script>
+<script setup>
 import api from '@/services/VerdeValleyRailApi';
+import { ref } from 'vue';
 
-var trip = await api.getTrip(111);
-alert("trips");
+
+
+
+var trips = ref([]);
+
+async function searchTrips() {
+    trips.value = (await api.searchTrips(null)).data;
+}
+
+searchTrips();
 
 </script>
 
 <template>
-  
-    <div>This is the trips vue</div>
-  
+    <div>
+        
+            
+            <v-table>
+                <thead>
+                    <tr>
+                        <th class="text-left">
+                            Departure
+                        </th>
+                        <th class="text-left">
+                            From
+                        </th>
+                        <th class="text-left">
+                            To
+                        </th>
+                        <th class="text-center">
+                            Minutes
+                        </th>
+                        <th class="text-center">
+                            Price
+                        </th>
+                        <th class="text-center">
+                            Available Seats
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="trip in trips" :key="trip.tripId">
+                        <td class="text-left">{{ trip.departure }}</td>
+                        <td class="text-left">{{ trip.startingStationName }}</td>
+                        <td class="text-left">{{ trip.endingStationName }}</td>
+                        <td class="text-center">{{ trip.minutes }}</td>
+                        <td class="text-center">{{ trip.pricePerSeat }}</td>
+                        <td class="text-center">{{ trip.availableSeats }}</td>
+                    </tr>
+                </tbody>
+            </v-table>
+        
+    </div>
 </template>
