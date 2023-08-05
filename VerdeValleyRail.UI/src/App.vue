56b53eb9c42882a8296fb628c421c16f0a60b9a6
Nathan Smith <nathpsmith@gmail.com>
2023-08-05 11:20:35 -0400

    Current state.  Need to figure out why routing isn't quite working.

--- VerdeValleyRail.UI/src/App.vue
+++ VerdeValleyRail.UI/src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/Trips">Trips</RouterLink>
       </nav>
     </div>
   </header>
---
+++ VerdeValleyRail.UI/src/views/TripsView.vue
@@ -0,0 +1,13 @@
+<script>
+import api from '@/services/VerdeValleyRailApi';
+
+var trip = await api.getTrip(111);
+alert("trips");
+
+</script>
+
+<template>
+  
+    <div>This is the trips vue</div>
+  
+</template>
